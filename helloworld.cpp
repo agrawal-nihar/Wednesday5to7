78ad4538594f00cc78cc97cc711fedd4e41638ab
Nihar Agrawal <ngagrawa@usc.edu>
2017-01-11 17:48:59 -0800

    Fight On!

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,8 @@
 int main (int argc, char* argv[])
 {
 	std:: cout << "Hello world!" << std::endl;
-	std:: cout << "cs 104 Rules!" << std:: endl;
-	
+	std:: cout << "cs 104 Rules!" << std:: endl;	
+	std:: cout << "Fight On!" << std:: endl;
+
 	return 0;
 }
